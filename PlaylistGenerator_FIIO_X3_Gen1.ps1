3cb5b5107e2abeee8c89eaa89ddee21e8d50c51e
unknown <_CMonahan@cmonahan-lap.ops.global.ad>
2016-03-30 22:31:00 -0400

    Initial add.

---
+++ PlaylistGenerator_FIIO_X3_Gen1.ps1
@@ -0,0 +1,10 @@
+﻿
+$SongList = Import-Csv -Path Songlist_X3_edited.csv
+$SongList | select -First 1
+$PlaylistsToMake = "Reggae"
+foreach ($PlaylistName in $PlaylistsToMake) {
+    Out-File -InputObject "#EXTM3U" -FilePath "$($env:TEMP)\g_$($PlaylistName).m3u8"
+    $SongList | Where-Object { $_.Genre -eq $PlaylistName } | Select-Object -ExpandProperty PathDest | Out-File -FilePath "$($env:TEMP)\g_$($PlaylistName).m3u8" -Append
+    gc "$($env:TEMP)\g_$($PlaylistName).m3u8"
+    Move-Item -Path "$($env:TEMP)\g_$($PlaylistName).m3u8" -Destination F:\
+}
