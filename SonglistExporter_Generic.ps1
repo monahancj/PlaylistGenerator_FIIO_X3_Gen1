aa4c27b215bb3159f9a81a2a696bf067d6f1e91c
monahancj <monahancj@gmail.com>
2018-04-20 18:18:22 -0400

    Updated variables for testing

--- SonglistExporter_Generic.ps1
+++ SonglistExporter_Generic.ps1
@@ -1,3 +1,5 @@
-﻿$FileList = Get-ChildItem -Path "F:\" -Include *.mp3,*.flac,*.m4a -Recurse | Select-Object -ExpandProperty FullName
-$SongList = $FileList | Select-Object @{n='PlayList';e={""}},@{n='Genre';e={""}},@{n='Artist';e={($_ -split("\\"))[-3]}},@{n='Album';e={($_ -split("\\"))[-2]}},@{n='PathDest';e={$_ -replace "F:\\","TF2:\"}},@{n='PathSource';e={$_}}
+﻿$MusicPath = "C:\Users\_cmonahan\Music\"
+$PlayerPath = "x:\"
+$FileList = Get-ChildItem -Path $MusicPath -Include *.mp3,*.flac,*.m4a -Recurse | Select-Object -ExpandProperty FullName
+$SongList = $FileList | Select-Object @{n='PlayList';e={""}},@{n='Genre';e={""}},@{n='Artist';e={($_ -split("\\"))[-3]}},@{n='Album';e={($_ -split("\\"))[-2]}},@{n='PathDest';e={$_ -replace "$($MusicPath)","$PlayerPath"}},@{n='PathSource';e={$_}}
 $SongList | Export-Csv -Path Songlist_X3.csv -NoTypeInformation
